91e708212a6253982675546e9921cd79db455794
daniel.kucko <daniel.kucko@freenet.de>
2018-05-11 13:56:34 +0200

    switched to function views, added list views

--- backend/armytool/armytools/urls.py
+++ backend/armytool/armytools/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('equipment/', views.EquipmentView.as_view(), name='equipment')
+    path('abilities/', views.get_ability_list),
+    path('armies/', views.get_army_list),
+    path('battlelogs/', views.get_battle_log_list),
+    path('equipment/', views.get_equipment_list),
+    path('models/', views.get_model_list),
+    path('modeltypes/', views.get_model_type_list),
+    path('players/', views.get_player_list),
+    path('scenarios/', views.get_scenario_list),
+    path('units/', views.get_unit_list),
 ]
--- backend/armytool/armytools/views.py
+++ backend/armytool/armytools/views.py
@@ -1,138 +1,48 @@
 from django.views import View
 from django.http import JsonResponse
-# from armytool.models import *
+from .models import *
 
 
-class BattleLogView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_equipment_list(request):
+    response = {'content': [{e.id: e.name} for e in Equipment.objects.all()]}
+    return JsonResponse(response)
 
 
-class ScenarioView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_model_list(request):
+    response = {'content': [{m.id: m.name} for m in Model.objects.all()]}
+    return JsonResponse(response)
 
 
-class PlayerArmyView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_ability_list(request):
+    response = {'content': [{a.id: a.name} for a in Ability.objects.all()]}
+    return JsonResponse(response)
 
 
-class PlayerView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_model_type_list(request):
+    response = {'content': [{mt.id: mt.name} for mt in ModelType.objects.all()]}
+    return JsonResponse(response)
 
 
-class ArmyView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_unit_list(request):
+    response = {'content': [{u.id: u.name} for u in Unit.objects.all()]}
+    return JsonResponse(response)
 
 
-class UnitView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_army_list(request):
+    response = {'content': [{a.id: a.name} for a in Army.objects.all()]}
+    return JsonResponse(response)
 
 
-class ModelTypeView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_battle_log_list(request):
+    response = {'content': [{b.id: b.name, 'date': b.date} for b in BattleLog.objects.all()]}
+    return JsonResponse(response)
 
 
-class ModelView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_player_list(request):
+    response = {'content': [{p.id: p.name} for p in Player.objects.all()]}
+    return JsonResponse(response)
 
 
-class EquipmentView(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
-
-    def update(self, request):
-        pass
-
-    def remove(self, request):
-        pass
+def get_scenario_list(request):
+    response = {'content': [{s.id: s.name} for s in Scenario.objects.all()]}
+    return JsonResponse(response)
