9d3eedcbaf54b48b42a36df466536570cfb9edd2
daniel.kucko <daniel.kucko@freenet.de>
2018-07-26 15:53:25 +0200

    Added buttons to add more equipment slots to model edit added unit edit screen

--- gui/editwidgets.py
+++ gui/editwidgets.py
@@ -1,6 +1,6 @@
 from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QWidget, QGroupBox, QFormLayout, QVBoxLayout, QLineEdit, QSpinBox, QLabel, QPushButton, \
-    QComboBox
+    QComboBox, QHBoxLayout
 
 
 class EditWidget(QGroupBox):
@@ -48,8 +48,12 @@ class ModelEdit(EditWidget):
         self.equipment_edit_list[0].addItems(self.items)
 
         self.form_group_layout.addRow(QLabel('Name:'), self.name_edit)
+        addButton = QPushButton('Add Equipment Slot')
+        addButton.clicked.connect(self.add_equipment)
         for ee in self.equipment_edit_list:
             self.form_group_layout.addRow(QLabel('Equipment:'), ee)
+        # TODO: Find better position for button
+        self.form_group_layout.addRow(QLabel(), addButton)
 
     def load_data(self, data):
         self.name_edit.setText(str(data['name']))
@@ -68,9 +72,81 @@ class ModelEdit(EditWidget):
 
         self.init_ui()
 
+    def add_equipment(self):
+        cb = QComboBox()
+        cb.addItems(self.items)
+        self.equipment_edit_list.append(cb)
+        index = self.form_group_layout.rowCount() - 2
+        self.form_group_layout.insertRow(index, QLabel('Add. Equipment:'), cb)
+
+
+class UnitEdit(EditWidget):
+
+    def __init__(self, data):
+        super(UnitEdit, self).__init__()
+        self.name_edit = QLineEdit()
+        self.model_edit_list = [(QComboBox(), QSpinBox())]
+        self.unit_type_edit = QComboBox()
+        self.might_edit = QSpinBox()
+        self.unit_types = [ut['name'] for ut in data['Unit types']]
+        self.unit_type_edit.addItems(self.unit_types)
+        self.items = [m['name'] for m in data['Models']]
+        self.model_edit_list[0][0].addItems(self.items)
+        add_button = QPushButton('Add Model')
+        add_button.clicked.connect(self.add_model)
+
+        self.form_group_layout.addRow(QLabel('Name:'), self.name_edit)
+        self.form_group_layout.addRow(QLabel('Unit type:'), self.unit_type_edit)
+        self.form_group_layout.addRow(QLabel('Might:'), self.might_edit)
+        for cb, sb in self.model_edit_list:
+            hbox = QHBoxLayout()
+            hbox.addWidget(cb)
+            hbox.addWidget(sb)
+            self.form_group_layout.addRow(QLabel('Model:'), hbox)
+        self.form_group_layout.addRow(QLabel(), add_button)
+
+    def load_data(self, data):
+        self.pk = data['id']
+        self.name_edit.setText(data['name'])
+        self.might_edit.setValue(data['might'])
+        ut_index = self.unit_type_edit.findText(data['unit_type'])
+        if ut_index >= 0:
+            self.unit_type_edit.setCurrentIndex(ut_index)
+        for i, v in enumerate(data['models']):
+            m, c = v
+            try:
+                self.model_edit_list[i]
+            except IndexError:
+                cb = QComboBox()
+                cb.addItems(self.items)
+                sb = QSpinBox()
+                self.model_edit_list.append((cb, sb))
+                hbox = QHBoxLayout()
+                hbox.addWidget(cb)
+                hbox.addWidget(sb)
+                self.form_group_layout.addRow(QLabel('Model:'), hbox)
+            index = self.model_edit_list[i][0].findText(m)
+            if index >= 0:
+                self.model_edit_list[i][0].setCurrentIndex(index)
+            self.model_edit_list[i][1].setValue(c)
+
+        self.init_ui()
+
+    def add_model(self):
+        cb = QComboBox()
+        cb.addItems(self.items)
+        sb = QSpinBox()
+        self.model_edit_list.append((cb, sb))
+        hbox = QHBoxLayout()
+        hbox.addWidget(cb)
+        hbox.addWidget(sb)
+        self.form_group_layout.insertRow(self.form_group_layout.rowCount() - 2, QLabel('Model:'), hbox)
+
 
 def get_edit_widget(typ, data):
     if typ == 'Models':
         return ModelEdit(data)
     elif typ == 'Equipment' or typ == 'Model types':
         return EquipmentEdit()
+    elif typ == 'Units':
+        return UnitEdit(data)
